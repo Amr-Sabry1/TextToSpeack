0565cbd10f921d7a0154a7547328aa90fe6257bf
Amr Sabry <95857185+Amr-Sabry1@users.noreply.github.com>
2023-05-31 13:23:41 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,42 @@
+let button = document.querySelector('button');
+let textSpeak = document.querySelector('input');
+let voiceSelect = document.getElementById('voice');
+
+
+
+function loadVoices() {
+    let voices = speechSynthesis.getVoices();
+
+    voices.forEach(function(voice, i) {
+        let option = document.createElement('option');
+
+        option.value = voice.name;
+        option.innerHTML = voice.name;
+
+        voiceSelect.appendChild(option);
+    });
+}
+
+
+window.speechSynthesis.onvoiceschanged = function(e) {
+    loadVoices();
+};
+
+function speak(text) {
+    let msg = new SpeechSynthesisUtterance();
+
+    msg.text = text;
+
+    if (voiceSelect.value) {
+        msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voiceSelect.value; })[0];
+    }
+
+    window.speechSynthesis.speak(msg);
+}
+
+
+button.addEventListener('click', function(e) {
+    if (textSpeak.value.length > 0) {
+        speak(textSpeak.value);
+    }
+});
